f0ccde9090783f16f462bbcd2b409faa0211d8cd
sbwml <admin@cooluc.com>
2023-05-30 16:34:58 +0800

    Revert "openwrt-23.05: add missing symbols in 5.15"
    It has been fixed https://github.com/openwrt/openwrt/commit/e2e7dd44506ee7e604a8b7c1a83837f1c025417f

--- openwrt/scripts/05-fix-source.sh
+++ openwrt/scripts/05-fix-source.sh
@@ -4,6 +4,3 @@
 # luci-app-firewall
 curl -s https://raw.githubusercontent.com/openwrt/luci/master/applications/luci-app-firewall/po/zh_Hans/firewall.po > feeds/luci/applications/luci-app-firewall/po/zh_Hans/firewall.po
 
-
-# openwrt-23.05
-[ "$version" = "snapshots-23.05" ] && echo "# CONFIG_X86_PLATFORM_DRIVERS_HP is not set" >> target/linux/generic/config-5.15
