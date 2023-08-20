1a94e589dbc65834531be848492ae9458a41c5b6
sbwml <admin@cooluc.com>
2023-08-21 05:04:36 +0800

    OpenWrt 23.05.0-rc3

--- tags/openwrt-tag.sh
+++ tags/openwrt-tag.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-curl -s "https://api.github.com/repos/openwrt/openwrt/tags" > /tmp/tags.json
+curl -s "https://api.github.com/repos/openwrt/openwrt/tags" -o tags.json || exit 1
 if [ ! -s /tmp/tags.json ]; then
     echo error
     exit 1
 else
-    cat /tmp/tags.json | jq . > /tmp/tags.json 2>&1
+    cat /tmp/tags.json | jq . > jq.json 2>&1
 fi
-[ $? -ne 0 ] && exit 1
 
-#grep name /tmp/tags.json | grep v22 | head -1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | sed 's/v//g' > v22
-grep name /tmp/tags.json | grep v23 | head -1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | sed 's/v//g' > v23
+grep name jq.json | grep v23 | head -1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | sed 's/v//g' > v23
+rm -f tags.json jq.json
