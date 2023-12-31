b1e29c3e243038a545bd336a92acef4a94393d78
sbwml <admin@cooluc.com>
2023-12-31 13:24:59 +0800

    perf: fix build for linux-6.6
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/scripts/05-fix-source.sh
+++ openwrt/scripts/05-fix-source.sh
@@ -27,3 +27,7 @@ popd
 
 # bpf - add host clang-15/17 support
 sed -i 's/command -v clang/command -v clang clang-17 clang-15/g' include/bpf.mk
+
+# perf
+curl -s https://$mirror/openwrt/patch/openwrt-6.x/musl/990-add-typedefs-for-Elf64_Relr-and-Elf32_Relr.patch > toolchain/musl/patches/990-add-typedefs-for-Elf64_Relr-and-Elf32_Relr.patch
+curl -s https://$mirror/openwrt/patch/openwrt-6.x/perf/Makefile > package/devel/perf/Makefile
