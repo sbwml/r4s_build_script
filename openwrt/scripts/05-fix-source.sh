4a491f39a3e14d4f1db14faec328397387fff3f9
sbwml <admin@cooluc.com>
2024-01-02 12:37:11 +0800

    grub2: disable `gc-sections` flag when CONFIG_USE_LTO is enabled
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/scripts/05-fix-source.sh
+++ openwrt/scripts/05-fix-source.sh
@@ -3,6 +3,9 @@
 # libsodium - fix build with lto (GNU BUG - 89147)
 sed -i "/CONFIGURE_ARGS/i\TARGET_CFLAGS += -ffat-lto-objects\n" feeds/packages/libs/libsodium/Makefile
 
+# grub2 -  disable `gc-sections` flag
+sed -i '/PKG_BUILD_FLAGS/ s/$/ no-gc-sections/' package/boot/grub2/Makefile
+
 # uqmi - fix gcc11
 if [ "$USE_GLIBC" != "y" ]; then
     sed -i '/dangling-pointer/d' package/network/utils/uqmi/Makefile
