82336b284ed01ceddcb35e8166c0563fef8a0b2e
sbwml <admin@cooluc.com>
2024-02-01 23:44:08 +0800

    patch: drop ksmdb patches
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/scripts/05-fix-source.sh
+++ openwrt/scripts/05-fix-source.sh
@@ -24,7 +24,6 @@ fi
 # ksmbd luci
 rm -rf feeds/luci/applications/luci-app-ksmbd
 cp -a ../master/luci/applications/luci-app-ksmbd feeds/luci/applications/luci-app-ksmbd
-curl -s https://$mirror/openwrt/patch/openwrt-6.x/ksmbd/version.patch | patch -p1
 sed -i 's/0666/0644/g;s/0777/0755/g' feeds/luci/applications/luci-app-ksmbd/htdocs/luci-static/resources/view/ksmbd.js
 
 # ksmbd tools
