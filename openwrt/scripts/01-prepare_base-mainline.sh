baa6eba86fc43de60ef4b194e966ab0db4fd7402
sbwml <admin@cooluc.com>
2024-03-07 06:48:57 +0800

    backport-6.6: tcp: reorganize tcp_sock fast path variables
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/scripts/01-prepare_base-mainline.sh
+++ openwrt/scripts/01-prepare_base-mainline.sh
@@ -178,6 +178,7 @@ curl -s https://$mirror/openwrt/patch/kernel-6.6/net/953-net-patch-linux-kernel-
 curl -s https://$mirror/openwrt/patch/kernel-6.6/backport/901-v6.8-cache-enforce-cache-groups.patch > target/linux/generic/backport-6.6/901-v6.8-cache-enforce-cache-groups.patch
 curl -s https://$mirror/openwrt/patch/kernel-6.6/backport/902-v6.8-netns-ipv4-reorganize-netns_ipv4-fast-path-variables.patch > target/linux/generic/backport-6.6/902-v6.8-netns-ipv4-reorganize-netns_ipv4-fast-path-variables.patch
 curl -s https://$mirror/openwrt/patch/kernel-6.6/backport/903-v6.8-net-device-reorganize-net_device-fast-path-variables.patch > target/linux/generic/backport-6.6/903-v6.8-net-device-reorganize-net_device-fast-path-variables.patch
+curl -s https://$mirror/openwrt/patch/kernel-6.6/backport/904-v6.8-tcp-reorganize-tcp_sock-fast-path-variables.patch > target/linux/generic/backport-6.6/904-v6.8-tcp-reorganize-tcp_sock-fast-path-variables.patch
 
 # ubnt-ledbar - fix linux-6.x
 rm -rf package/kernel/ubnt-ledbar
