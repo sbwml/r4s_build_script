927c20e357a4d3b5f77f3abf23e558656524871f
sbwml <admin@cooluc.com>
2024-04-07 08:49:07 +0800

    config: update config-musl-r8500
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -335,7 +335,7 @@ fi
 [ "$ENABLE_UHTTPD" = "y" ] && sed -i '/nginx/d' .config && echo 'CONFIG_PACKAGE_ariang=y' >> .config
 
 # bcm53xx: upx_list.txt
-[ "$platform" = "bcm53xx" ] && curl -s https://$mirror/openwrt/generic/upx_list.txt > upx_list.txt
+# [ "$platform" = "bcm53xx" ] && curl -s https://$mirror/openwrt/generic/upx_list.txt > upx_list.txt
 
 # Toolchain Cache
 if [ "$BUILD_FAST" = "y" ]; then
