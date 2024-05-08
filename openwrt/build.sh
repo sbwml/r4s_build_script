7e9a4f6e0a50bbdeb5399c0cc0354d235bc7e2da
sbwml <admin@cooluc.com>
2024-05-08 19:20:41 +0800

    fix lan
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -95,7 +95,7 @@ elif [ "$1" = "rc2" ]; then
 fi
 
 # lan
-[ -n "$LAN" ] && export LAN=$LAN
+[ -n "$LAN" ] && export LAN=$LAN || export LAN=10.0.0.1
 
 # platform
 [ "$2" = "nanopi-r4s" ] && export platform="rk3399" toolchain_arch="nanopi-r4s"
