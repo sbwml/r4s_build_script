4e76d5dcf7a8ad30fa30f27d9492515ff048c853
sbwml <admin@cooluc.com>
2024-07-08 18:14:34 +0800

    public: fix kernel patches
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/scripts/01-prepare_base-mainline.sh
+++ openwrt/scripts/01-prepare_base-mainline.sh
@@ -35,8 +35,8 @@ grep HASH include/kernel-6.6 | awk -F'HASH-' '{print $2}' | awk '{print $1}' | m
 
 # kernel generic patches
 rm -rf target/linux/generic
-kernel_version=$(grep HASH include/kernel-6.6 | awk -F'HASH-' '{print $2}' | awk '{print $1}')
-release_kernel_version=$(curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/master/tags/kernel-6.6 | awk -F'HASH-' '{print $2}' | awk '{print $1}')
+kernel_version=$(sed -n 's/^LINUX_KERNEL_HASH-\([0-9.]\+\) = .*/\1/p' include/kernel-6.6)
+release_kernel_version=$(curl -sL https://raw.githubusercontent.com/sbwml/r4s_build_script/master/tags/kernel-6.6 | sed -n 's/^LINUX_KERNEL_HASH-\([0-9.]\+\) = .*/\1/p')
 if [ "$kernel_version" = "$release_kernel_version" ]; then
     git clone https://$github/sbwml/target_linux_generic -b main target/linux/generic --depth=1
 else
