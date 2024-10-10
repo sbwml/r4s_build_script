6fe39f7e7ee4f7837469222643a7abd3c78886d0
sbwml <admin@cooluc.com>
2024-10-11 04:46:47 +0800

    build.sh: fix typo
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -259,7 +259,7 @@ curl -sO https://$mirror/openwrt/scripts/03-convert_translation.sh
 curl -sO https://$mirror/openwrt/scripts/04-fix_kmod.sh
 curl -sO https://$mirror/openwrt/scripts/05-fix-source.sh
 curl -sO https://$mirror/openwrt/scripts/99_clean_build_cache.sh
-if [ -z "$git_password" ] && [ -z "$private_url" ]; then
+if [ -n "$git_password" ] && [ -n "$private_url" ]; then
     curl -u openwrt:$git_password -sO "$private_url"
 else
     curl -sO https://$mirror/openwrt/scripts/10-custom.sh
