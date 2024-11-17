65dc3a3e512a7f547add7c8d49e6b53ece38b5a5
sbwml <admin@cooluc.com>
2024-11-17 22:13:35 +0800

    mt76: update from openwrt upstream
    Signed-off-by: sbwml <admin@cooluc.com>

---
+++ openwrt/patch/mt76/README.md
@@ -0,0 +1,1 @@
+# mt76 - fix work for linux-6.12
--- openwrt/scripts/01-prepare_base-mainline.sh
+++ openwrt/scripts/01-prepare_base-mainline.sh
@@ -171,9 +171,7 @@ git clone https://$github/sbwml/package_kernel_rtl8812au-ct package/kernel/rtl88
 git clone https://$github/sbwml/package_kernel_rtl8812au-ac package/kernel/rtl8812au-ac -b v6.11
 
 # mt76 - 2024-10-11
-rm -rf package/kernel/mt76
 mkdir -p package/kernel/mt76/patches
-curl -s https://$mirror/openwrt/patch/mt76/Makefile > package/kernel/mt76/Makefile
 curl -s https://$mirror/openwrt/patch/mt76/patches/100-fix-build-with-mac80211-6.11-backport.patch > package/kernel/mt76/patches/100-fix-build-with-mac80211-6.11-backport.patch
 curl -s https://$mirror/openwrt/patch/mt76/patches/101-fix-build-with-linux-6.12rc2.patch > package/kernel/mt76/patches/101-fix-build-with-linux-6.12rc2.patch
 
