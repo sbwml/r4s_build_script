b20569f3b2fcbac71df472ac043d45cc97a1489e
sbwml <admin@cooluc.com>
2025-02-27 22:53:54 +0800

    linux-6.12: bump to 6.12.17
    Signed-off-by: sbwml <admin@cooluc.com>

--- tags/kernel-tag.sh
+++ tags/kernel-tag.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 ROOT="./"
 
 # LTS
-KERNEL_VERSION=`curl -s https://cdn.kernel.org/pub/linux/kernel/v6.x/sha256sums.asc | awk '{print $2}' | grep -E ^linux-6.12 | grep tar.xz | sed 's/linux-//g;s/.tar.xz//g' | tail -n 1`
-KERNEL_HASH=`curl -s https://cdn.kernel.org/pub/linux/kernel/v6.x/sha256sums.asc | grep linux-$KERNEL_VERSION | grep tar.xz | awk '{print $1}'`
+KERNEL_VERSION=`curl -s https://raw.githubusercontent.com/andreoss/kernel-overlay/refs/heads/master/sources.json | jq -r '.[] | select(.package.name == "6_12") | .version'`
+KERNEL_HASH=`curl -s https://raw.githubusercontent.com/andreoss/kernel-overlay/refs/heads/master/sources.json | jq -r '.[] | select(.package.name == "6_12") | .checksum'`
 TAG=`echo $KERNEL_VERSION | awk -F"." '{print $3}'`
 
 [ -z $TAG ] && TAG="" || TAG=.$TAG
