400af6976df113d565b3b4a534043172f27a17fe
sbwml <admin@cooluc.com>
2025-04-01 20:48:20 +0800

    natmap: disable syslogs by default
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -57,6 +57,7 @@ curl -s $mirror/openwrt/patch/luci/applications/luci-app-frpc/001-luci-app-frpc-
 curl -s $mirror/openwrt/patch/luci/applications/luci-app-frpc/002-luci-app-frpc-add-enable-flag.patch | patch -p1
 
 # natmap
+sed -i 's/log_stdout:bool:1/log_stdout:bool:0/g;s/log_stderr:bool:1/log_stderr:bool:0/g' feeds/packages/net/natmap/files/natmap.init
 pushd feeds/luci
     curl -s $mirror/openwrt/patch/luci/applications/luci-app-natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch | patch -p1
 popd
