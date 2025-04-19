86c90c1c81928fdf71e1f68c1075c03149c98a85
sbwml <admin@cooluc.com>
2025-04-20 00:56:21 +0800

    x86: add RTL8127 PCI 10 gigabit ethernet driver
    Signed-off-by: sbwml <admin@cooluc.com>

--- openwrt/scripts/00-prepare_base.sh
+++ openwrt/scripts/00-prepare_base.sh
@@ -49,13 +49,14 @@ if [ "$ENABLE_UHTTPD" != "y" ]; then
     fi
 fi
 
-# Realtek driver - R8168 & R8125 & R8126 & R8152 & R8101
-rm -rf package/kernel/r8168 package/kernel/r8101 package/kernel/r8125 package/kernel/r8126
+# Realtek driver - R8168 & R8125 & R8126 & R8152 & R8101 & r8127
+rm -rf package/kernel/{r8168,r8101,r8125,r8126,r8127}
 git clone https://$github/sbwml/package_kernel_r8168 package/kernel/r8168
 git clone https://$github/sbwml/package_kernel_r8152 package/kernel/r8152
 git clone https://$github/sbwml/package_kernel_r8101 package/kernel/r8101
 git clone https://$github/sbwml/package_kernel_r8125 package/kernel/r8125
 git clone https://$github/sbwml/package_kernel_r8126 package/kernel/r8126
+git clone https://$github/sbwml/package_kernel_r8127 package/kernel/r8127
 
 # GCC Optimization level -O3
 if [ "$platform" = "x86_64" ]; then
